6c8c5fff7030dd932b97c70d603a84aa8fc73947
Zer0Calib3r <82219229+JfksAssociate@users.noreply.github.com>
2023-04-30 17:36:43 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function launch() {
+    window.location.href='minecraft://?addServer=monman11.com:25606';
+    navigator.clipboard.writeText("monman11.com");
+    var e = document.querySelector("#launch");
+    e.innerText = "Copied!";
+
+}
