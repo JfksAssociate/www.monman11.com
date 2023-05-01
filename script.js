0cd298ea6a6c8d7c41e9e833bda69f7d5401ce70
Zer0Calib3r <82219229+JfksAssociate@users.noreply.github.com>
2023-05-01 08:56:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ function launch() {
     navigator.clipboard.writeText("monman11.com");
     var e = document.querySelector("#launch");
     e.innerText = "Copied!";
-
+}
+//JQUERY ios scrolling
+const isMobileiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+if (window.innerWidth <= 767) {
+$(window).scroll(function() {
+    var scrolledY = $(window).scrollTop();
+    $('#container').css('background-position', 'left ' + ((scrolledY)) + 'px');
+  });
 }
